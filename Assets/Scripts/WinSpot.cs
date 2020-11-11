b3db68a1803a239387b1330dd3ce0eca325376d5
Mark <mark_kaldas@yahoo.com>
2020-11-11 15:55:44 -0700

    nonsense

--- Assets/Scripts/WinSpot.cs
+++ Assets/Scripts/WinSpot.cs
@@ -12,8 +12,10 @@ public class WinSpot : MonoBehaviour
     public List<GameObject> containedCats;
     public Text catLabel;
     public Text timerLabel;
+    public Text timeLeftLabel;
 
     public float timer = 0f;
+    public float timeLeft = 300f;
     private GameObject player;
     // The time all the cats have to be in the circle for
     public float timeToWin = 10;
@@ -32,7 +34,7 @@ public class WinSpot : MonoBehaviour
             instance.transform.position = Random.insideUnitCircle * catSpawnRadius;
             var cat = instance.GetComponent<CatMovement>();
             cat.dog = player;
-            cat.annoyingness = 1f - CircleProbability();
+            cat.annoyingness = 1.2f - CircleProbability();
         }
         allCats = GameObject.FindGameObjectsWithTag("Cat");
     }
@@ -51,20 +53,28 @@ public class WinSpot : MonoBehaviour
     }
 
     void Update() {
-        if(!hasWon && allCats.Length == containedCats.Count) {
-            timer += Time.deltaTime;
+        if(hasWon) return;
+        timeLeft -= Time.deltaTime;
+        // if(!hasWon && allCats.Length == containedCats.Count) {
+        timer += Time.deltaTime * (1f / Mathf.Pow(2, allCats.Length - containedCats.Count));
 
-            if(timer >= timeToWin) {
-                hasWon = true;
-            }
-        }
-        else {
-            timer = 0f;
-        }
+            // if(timer >= timeToWin) {
+            //     hasWon = true;
+            // }
+        // }
+        // else {
+        //     timer = 0f;
+        // }
 
         catLabel.text = containedCats.Count + " / " + allCats.Length;
 
-        timerLabel.gameObject.SetActive(!hasWon && allCats.Length == containedCats.Count && (timer % 1.0f < 0.75f));
-        timerLabel.text = Mathf.Ceil(10f - timer) + "s";
+        // timerLabel.gameObject.SetActive(!hasWon && allCats.Length == containedCats.Count && (timer % 1.0f < 0.75f));
+        // timerLabel.text = Mathf.Ceil(10f - timer) + "s";
+        timerLabel.text = Mathf.Floor(timer) + "%";
+        timeLeftLabel.text = Mathf.Ceil(timeLeft) + "s Left";
+
+        if(timer >= 100) {
+            hasWon = true;
+        }
     }
 }
